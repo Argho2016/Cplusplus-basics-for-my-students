0716e43936eabebd1e6a48b697e6a6b72cb886ea
Argho Das <aiub2016@yahoo.com>
2021-10-13 20:37:07 +0600

    Array problem solving

---
+++ lab 3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int x = 1;
+
+    do{
+        cout << x << endl;
+        x++;
+
+    }while(x<0);
+    */
+
+    //int Loki[4] = {11, 22, 33, 44}; //Array
+    //cout << Loki[0] << endl;
+
+    /*int Rick[9];
+
+    for (int x = 0; x <=8; x++)
+    {
+        Rick[x] = 9;
+
+        cout << x << Rick[x] << endl;*/
+
+
+        int Redhood [5] = {10, 50, 37, 43, 500};
+
+        int sum = 0;
+
+        for (int x = 0; x <5; x++){
+            sum = sum + Redhood[x];
+            //sum = 60 + 50;
+            cout << sum << endl;
+        }
+
+    }
+
+
